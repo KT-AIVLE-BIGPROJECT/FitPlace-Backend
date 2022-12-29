5524f9547e9580ed711b631c0e9a8ba639008b0b
kybeen <rei980627@gmail.com>
2022-12-30 00:52:06 +0900

    (22.12.30) - 추천화면 필터링 프론트에 맞게 구현

--- places/views.py
+++ places/views.py
@@ -50,7 +50,7 @@ class RecommendationAPI(APIView):
             return Response(0)
         else:
             main_category = body["main_category"]
-            # mid_category = body["mid_category"]
+            # sub_category = body["sub_category"]
             filter_rating = body["filter_rating"]
             filter_review = body["filter_review"]
             filter_region = body["filter_region"]
@@ -118,6 +118,12 @@ class RecommendationAPI(APIView):
                                                         (final_recommends['search_category']=="거리")]
             else:
                 pass
+
+            #### 중분류 카테고리(키워드) 선택
+            # if sub_category == "":
+            #     pass
+            # elif:
+            #     final_recommends_response = final_recommends_response.loc[final_recommends_response['search_category'] == sub_category]
             
             #### 평점 필터링
             if filter_rating == 1: # 높은 순
